fd532a109604691655f5b5eed7f128c3258b9380
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2025-02-07 04:24:57 +0100

    feat: add framer-motion for enhanced animations and update BrowserRouter for future compatibility

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ globalStyles();
 
 const App = () => (
   <ThemeProvider>
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <AppRouter />
     </BrowserRouter>
   </ThemeProvider>
