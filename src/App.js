827e21ac6ac1fdba0264c03b965baa7fb4c1d3b8
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2025-02-07 21:38:19 +0100

    feat: enhance app performance with code splitting and lazy loading, update index.html for better SEO

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-// App.js
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter } from "react-router-dom";
-import AppRouter from "../src/routes/AppRouter";
 import { globalStyles } from "./styles/globalStyles";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import Spinner from "./components/common/spinner/Spinner";
+
+const AppRouter = lazy(() => import("./routes/AppRouter"));
 
 globalStyles();
 
 const App = () => (
   <ThemeProvider>
     <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-      <AppRouter />
+      <Suspense fallback={<Spinner />}>
+        <AppRouter />
+      </Suspense>
     </BrowserRouter>
   </ThemeProvider>
 );
--- webpack.config.js
+++ webpack.config.js
@@ -10,23 +10,32 @@ module.exports = {
   optimization: {
     splitChunks: {
       chunks: "all",
+      maxInitialRequests: Infinity, // Increases the number of parallel requests at entry
+      minSize: 0, // Minimum chunk size in bytes
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
+          name(module) {
+            // Grabs the folder name of the package
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+            // Avoids vendor prefix and better naming for chunks
+            return `npm.${packageName.replace("@", "")}`;
+          },
           chunks: "all",
         },
       },
     },
   },
-  plugins: [new BundleAnalyzerPlugin()],
+  plugins: [
+    new BundleAnalyzerPlugin(), // Helps to visualize size of webpack output files with an interactive zoomable treemap.
+  ],
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.jsx?$/, // Transpile .js and .jsx files
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: "babel-loader", // Use babel-loader for these files
         },
       },
     ],
